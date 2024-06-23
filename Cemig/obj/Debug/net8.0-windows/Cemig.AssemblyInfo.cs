92497ab2a9800d3101af0939edc17b62923ac5ae
michael pires <128551672+mayckit@users.noreply.github.com>
2024-06-23 16:51:52 -0300

    Classe de usuarios e atualizações em cadastro de usuario

--- Cemig/obj/Debug/net8.0-windows/Cemig.AssemblyInfo.cs
+++ Cemig/obj/Debug/net8.0-windows/Cemig.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cemig")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80d2df83489afc92d19a2a956831a3781c93f290")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04e78f78d9333a88492e89fcaca08756b1710a50")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cemig")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cemig")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
