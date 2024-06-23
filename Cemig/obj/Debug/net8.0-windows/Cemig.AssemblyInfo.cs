80d2df83489afc92d19a2a956831a3781c93f290
Gabriel Paiva <1470350@sga.pucminas.br>
2024-06-23 11:27:55 -0300

    Apagado pasta arquivo

--- Cemig/obj/Debug/net8.0-windows/Cemig.AssemblyInfo.cs
+++ Cemig/obj/Debug/net8.0-windows/Cemig.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cemig")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f470fefe2d298b4725d1a5319fe88170790ecf90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aff9d434cf3094171a8dd72abeacaf0e907f76a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cemig")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cemig")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
